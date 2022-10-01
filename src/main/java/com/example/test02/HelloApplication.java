069e691a1aabe8fa29597087d9436ed4297e7ae3
NapMarta <marta.napolillo@outlook.it>
2022-10-01 11:49:42 +0200

    quinta commit sul branch master (fork da Giorgione) NB. le modifiche sulla fork non cambiano il progetto di Giorgione

--- src/main/java/com/example/test02/HelloApplication.java
+++ src/main/java/com/example/test02/HelloApplication.java
@@ -22,6 +22,7 @@ public class HelloApplication extends Application {
         System.out.println("Hello world!");
         System.out.println("Ciao world!");
         System.out.println("Ciao world!");
+        System.out.println("Ciao mondo!!!!!!!!!");
         launch();
     }
 }
